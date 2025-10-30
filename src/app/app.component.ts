bb5a9da7863f0468bdf8aea6ff6378f91980d9fe
vishaka-dev <vishakarajapaksha97@gmail.com>
2025-10-30 18:33:21 +0530

    Portfolio barely complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-root', // used to identify this component in HTML
+  //imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, NavComponent, RouterOutlet]
 })
 export class AppComponent {
-  title = 'anguler_website';
+  title = 'My angular portfolio';
 }
---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ProjectCardComponent } from "../project-card/project-card.component";
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [ProjectCardComponent],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.css'
+})
+export class PortfolioComponent {
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("Vishaka-Portfolio");
+  }
+}
